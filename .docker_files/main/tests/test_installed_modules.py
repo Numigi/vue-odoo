28e08ab703de1ee1878dca3fc44a2fcb9e9a288d
David Dufresne <27902736+ddufresne@users.noreply.github.com>
2020-07-31 09:21:28 -0400

    Add dummy tests

---
+++ .docker_files/main/tests/test_installed_modules.py
@@ -0,0 +1,13 @@
+
+from odoo.tests import TransactionCase
+
+
+class TestModules(TransactionCase):
+
+    def setUp(self):
+        super(TestModules, self).setUp()
+        self.modules = self.env['ir.module.module']
+
+    def test_vue_stock_forcast(self):
+        module = self.modules.search([('name', '=', 'vue_stock_forcast')])
+        self.assertTrue(module.state == "installed")
