224ab458e08092adcd1c37b6169b81a8052ade88
David Dufresne <27902736+ddufresne@users.noreply.github.com>
2020-07-31 09:48:16 -0400

    Vue router (#6)
    * TA#25773 Add module vue_router

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,10 @@ FROM quay.io/numigi/odoo-public:12.latest
 MAINTAINER numigi <contact@numigi.com>
 
 COPY vue /mnt/extra-addons/vue
+COPY vue_backend /mnt/extra-addons/vue_backend
 COPY vue_element_ui /mnt/extra-addons/vue_element_ui
+COPY vue_frontend /mnt/extra-addons/vue_frontend
+COPY vue_router /mnt/extra-addons/vue_router
 COPY vue_stock_forecast /mnt/extra-addons/vue_stock_forecast
 
 COPY .docker_files/main /mnt/extra-addons/main
---
+++ vue_frontend/__manifest__.py
@@ -0,0 +1,16 @@
+# © 2020 Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+{
+    "name": "Vuejs Frontend",
+    "version": "1.1.0",
+    "author": "Numigi",
+    "maintainer": "Numigi",
+    "website": "https://bit.ly/numigi-com",
+    "license": "LGPL-3",
+    "category": "Web",
+    "summary": "Add vuejs assets to the frontend of Odoo.",
+    "depends": ["vue"],
+    "data": ["views/assets.xml"],
+    "installable": True,
+}
