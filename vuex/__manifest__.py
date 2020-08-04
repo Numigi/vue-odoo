051a27bc818b7213bdcfff896f264ab1a2b96a9c
David Dufresne <27902736+ddufresne@users.noreply.github.com>
2020-08-04 11:59:00 -0400

    Vuex (#16)
    * Add vuex

---
+++ vuex/__manifest__.py
@@ -0,0 +1,16 @@
+# © 2020 Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+{
+    'name': 'Vuex',
+    'version': '1.0.0',
+    'author': 'Numigi',
+    'maintainer': 'Numigi',
+    'website': 'https://bit.ly/numigi-com',
+    'license': 'LGPL-3',
+    'category': 'Web',
+    'summary': 'Integrate vuex with Odoo frontend.',
+    'depends': ['vue_frontend'],
+    'data': ['views/assets.xml'],
+    'installable': True,
+}
---
+++ vuex/static/src/js/store.js
@@ -0,0 +1,9 @@
+odoo.define("vuex.store", function (require) {
+"use strict";
+
+const Class = require("web.Class");
+
+Vue.use(Vuex)
+return new Vuex.Store({ modules: [] });
+
+});
